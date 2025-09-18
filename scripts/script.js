28587d1a7f9a14265512e86c0380f547c1c9ae96
majid-bukhari <majidusman1170@gmail.com>
2025-09-18 19:36:09 +0500

    sixth comment

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,16 @@
   menuBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("hidden");
   });
+//  for hero slide show
+  const slides = document.querySelectorAll("#slideshow img");
+  let index = 0;
+
+  setInterval(() => {
+    slides[index].classList.remove("opacity-100");
+    slides[index].classList.add("opacity-0");
+
+    index = (index + 1) % slides.length;
+
+    slides[index].classList.remove("opacity-0");
+    slides[index].classList.add("opacity-100");
+  }, 3000); // 5 seconds per slide
