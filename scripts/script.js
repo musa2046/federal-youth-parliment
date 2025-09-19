7a155160793f1dfcf8368569fa87915fdfb15297
majid-bukhari <majidusman1170@gmail.com>
2025-09-19 19:38:32 +0500

    eigth commit

--- scripts/script.js
+++ scripts/script.js
@@ -16,4 +16,18 @@
 
     slides[index].classList.remove("opacity-0");
     slides[index].classList.add("opacity-100");
-  }, 3000); // 5 seconds per slide
+  }, 3000);
+
+  // for news section
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: { delay: 3000 },
+    pagination: { el: ".swiper-pagination", clickable: true },
+    navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+    breakpoints: {
+      640: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 }
+    }
+  });
