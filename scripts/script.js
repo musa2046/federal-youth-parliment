3327f2987d29cc23e9c02321f95db66410b79f87
musa2046 <musa2046@gmail.com>
2025-09-26 11:45:52 +0500

    fixed footer date to change dynamically

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,40 @@
 // Toggle mobile menu
-  const menuBtn = document.getElementById("mobileMenuBtn");
-  const mobileMenu = document.getElementById("mobileMenu");
-  menuBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
-  });
+const menuBtn = document.getElementById("mobileMenuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
 //  for hero slide show
-  const slides = document.querySelectorAll("#slideshow img");
-  let index = 0;
+const slides = document.querySelectorAll("#slideshow img");
+let index = 0;
 
-  setInterval(() => {
-    slides[index].classList.remove("opacity-100");
-    slides[index].classList.add("opacity-0");
-
-    index = (index + 1) % slides.length;
-
-    slides[index].classList.remove("opacity-0");
-    slides[index].classList.add("opacity-100");
-  }, 3000);
-
-  // for news section
+setInterval(() => {
+  slides[index].classList.remove("opacity-100");
+  slides[index].classList.add("opacity-0");
+  
+  index = (index + 1) % slides.length;
+  
+  slides[index].classList.remove("opacity-0");
+  slides[index].classList.add("opacity-100");
+}, 3000);
+ const year = new Date().getFullYear()
+      
+      var footerYear = document.querySelectorAll('.footerYear')
+      console.log(footerYear)
+      footerYear.forEach((e)=>{
+        e.innerHTML = year;
+      })
+// for news section
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    autoplay: { delay: 3000 },
-    pagination: { el: ".swiper-pagination", clickable: true },
-    navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
-    breakpoints: {
-      640: { slidesPerView: 2 },
-      1024: { slidesPerView: 3 }
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: { delay: 3000 },
+  pagination: { el: ".swiper-pagination", clickable: true },
+  navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+  breakpoints: {
+    640: { slidesPerView: 2 },
+    1024: { slidesPerView: 3 }
     }
   });
-  
-
+ 
